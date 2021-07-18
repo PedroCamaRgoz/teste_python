ff044a974d1a424359bb59256cd0f86fe950e3a2
Pedro <pedroels297@gmail.com>
2021-07-18 18:05:48 -0300

    funções para listas em python

---
+++ teste_2.py
@@ -0,0 +1,22 @@
+pets =['cao','gato','rato']
+
+pest2 = ('rato','egua','boi')
+
+pets[0]="sapo"
+
+
+print(pets)
+
+pets.count('gato')
+
+print(pets.count('gato'))
+
+print(pets.index('rato'))
+
+pets.insert(3,"cavalo")
+
+pets.sort()
+
+
+
+print(pets)
