600fdceb4a38e8041a46b2d45559d351128728ff
Pedro <pedroels297@gmail.com>
2021-07-18 17:25:32 -0300

    Primeiro commit

---
+++ teste.py
@@ -0,0 +1,11 @@
+# temp =  input("qual é o seu nome ?")
+
+#print("seu nome é %s, bem legal" %(temp))
+
+def teste (*nome):
+    print ("um teste : ")
+    for nome in nome:
+        print(nome)
+
+teste("pedro", "ana","lucio","italo","beatriz")
+
